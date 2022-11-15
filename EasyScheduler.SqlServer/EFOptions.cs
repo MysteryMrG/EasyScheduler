a7c6f1a016242b588a6b58e418fd527dff82ec3f
苟明红 <gou-minghon@hotmail.com>
2022-11-15 16:49:49 +0800

    sample

---
+++ EasyScheduler.SqlServer/EFOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EasyScheduler.SqlServer
+{
+    public class EfOptions
+    {
+        public const string DefaultSchema = "Es";
+
+        /// <summary>
+        /// Gets or sets the schema to use when creating database objects.
+        /// </summary>
+        public string Schema { get; set; } = DefaultSchema;
+
+        /// <summary>
+        /// EF dbcontext type.
+        /// </summary>
+        internal Type DbContextType { get; set; }
+    }
+}
---
+++ EasyScheduler.SqlServer/SqlServerOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyScheduler.SqlServer
+{
+    public class SqlServerOptions : EfOptions
+    {
+        /// <summary>
+        /// Gets or sets the database's connection string that will be used to store database entities.
+        /// </summary>
+        public string ConnectionString { get; set; }
+    }
+}
