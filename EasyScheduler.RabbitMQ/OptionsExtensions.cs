67b346b39fc296c37d3c801dc9c1fcbb1696f5d6
苟明红 <gou-minghon@hotmail.com>
2022-11-29 18:02:34 +0800

    add MQ

---
+++ EasyScheduler.RabbitMQ/OptionsExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EasyScheduler;
+using EasyScheduler.RabbitMQ;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class OptionsExtensions
+    {
+        // ReSharper disable once InconsistentNaming
+        public static SchedulerOptions UseRabbitMQ(this SchedulerOptions options, string hostName)
+        {
+            return options.UseRabbitMQ(opt => { opt.HostName = hostName; });
+        }
+
+        // ReSharper disable once InconsistentNaming
+        public static SchedulerOptions UseRabbitMQ(this SchedulerOptions options, Action<RabbitMQOptions> configure)
+        {
+            if (configure == null)
+            {
+                throw new ArgumentNullException(nameof(configure));
+            }
+
+            options.RegisterExtension(new RabbitMQOptionsExtension(configure));
+
+            return options;
+        }
+
+    }
+}
---
+++ EasyScheduler.RabbitMQ/RabbitMQOptionsExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasyScheduler.RabbitMQ
+{
+    internal sealed class RabbitMQOptionsExtension : IOptionsExtension
+    {
+        private readonly Action<RabbitMQOptions> _configure;
+
+        public RabbitMQOptionsExtension(Action<RabbitMQOptions> configure)
+        {
+            _configure = configure;
+        }
+
+        public void AddServices(IServiceCollection services)
+        {
+            //services.AddSingleton<CapMessageQueueMakerService>();
+
+            var options = new RabbitMQOptions();
+            _configure?.Invoke(options);
+            services.AddSingleton(options);
+
+            //services.AddSingleton<IConsumerClientFactory, RabbitMQConsumerClientFactory>();
+            //services.AddSingleton<IConnectionChannelPool, ConnectionChannelPool>();
+            //services.AddSingleton<IPublishExecutor, RabbitMQPublishMessageSender>();
+            //services.AddSingleton<IPublishMessageSender, RabbitMQPublishMessageSender>();
+        }
+    }
+}
